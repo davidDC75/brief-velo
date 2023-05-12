cd9edab358bd1a61d9d8dde271840d38be5c11ee
David De Castro <segphault@gmail.com>
2023-05-12 15:29:38 +0200

    Premier pas avec leaflet et lecture gpx

--- js/leaflet.js
+++ js/leaflet.js
@@ -5,20 +5,49 @@ let circleOpacity = 1;
 
 let map = L.map('map').setView([50.237, 2.758], 8);
 
+let circlesTbl = new Array();
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// L.marker([50.237, 2.758]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
+// let circle = L.circle([50.237,2.758], {
+//     color: circleColor,
+//     fillColor: circleFillColor,
+//     fillOpacity: circleOpacity,
+//     radius: circleRadius
+// }).addTo(map);
 
-let circle = L.circle([50.237,2.758], {
-    color: circleColor,
-    fillColor: circleFillColor,
-    fillOpacity: circleOpacity,
-    radius: circleRadius
-}).addTo(map);
+// circle.bindPopup('I am a circle');
 
-circle.bindPopup('I am a circle');
+let gpx = './js/trace-gpx.xml';
+
+new L.GPX(gpx, {
+        async: true,
+        gpx_options: {
+            joinTrackSegments: false
+        },
+        polyline_options: { // Couleur et épaisseur de la ligne des tracés
+            color: '#F59C00',
+            opacity: 0.75,
+            weight: 3,
+            lineCap: 'round'
+        },
+        marker_options: {
+            shadowUrl: '',
+            startIcon: new L.divIcon({
+                html: '<div class="map-marker"></div>'            
+            }),
+            endIcon: new L.divIcon({
+                html: '<div class="map-marker"></div>'            
+            })
+         
+        }
+
+    }).on('loaded', (e) => {
+        map.fitBounds(e.target.getBounds());
+    }).on('addpoint', (e) => {
+        console.log('Added '+e.point_type+' point: '+e.point);
+    })
+    .addTo(map);
